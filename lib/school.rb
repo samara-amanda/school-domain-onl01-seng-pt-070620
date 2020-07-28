79a40743fe5c14e152fac7dd1985d57a9e6aabf3
Samara Grant <samara.amanda01@gmail.com>
2020-07-28 01:47:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :grade, :roster
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+    # if roster[grade]
+    #   roster[grade] << name
+    # else
+    #   roster[grade] = []
+    #   roster[grade] << name
+    # end
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+
+end
